ffdc1296347a20c4b6af86fe38f32341d7e232ee
DarkmoonOnDiscord <121457786+DarkmoonOnDiscord@users.noreply.github.com>
2023-12-04 21:14:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write(`TOP 5 REASONS WHY YOU SHOULD KYS :
+    N°1 : ur ugly
+    N°2 : theres a bridge by your house
+    N°3 : 198.161.9.1
+    N°4 : u haz no friendz😂😂😂
+    N°5 : lol`);
     res.end();
 }).listen(process.env.PORT || 3000);
