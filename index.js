5c258a9e6bb0b9be2ee25712368c2a744cf56da7
DarkmoonOnDiscord <121457786+DarkmoonOnDiscord@users.noreply.github.com>
2023-12-06 21:42:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,157 @@ http.createServer(function (req, res) {
     N°5 : lol`);
     res.end();
 }).listen(process.env.PORT || 3000);
+
+
+const { AoiClient, LoadCommands } = require("aoi.js");
+const { AoiVoice, PlayerEvents, PluginName, Cacher, Filter } = require("@akarui/aoi.music");
+
+const bot = new AoiClient({
+    token: "MTE0MzY3NDI2NTEyMDU0MjgyMQ.GN2pNj.1i4mUSxQOLbEZg0P-uM1UVDi0GyB1gJYnZnj74",
+    prefix: "+",
+    intents: ["Guilds", "GuildMessages", "MessageContent", "GuildVoiceStates"],
+    events: ["onMessage", "onInteractionCreate",],
+    database: {
+        type: "aoi.db",
+        db: require("@akarui/aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+const voice = new AoiVoice(bot, {
+    searchOptions: {
+        soundcloudClientId: "Soundcloud ID", // optional
+        youtubegl: "US",
+    },
+    requestOptions: {
+        offsetTimeout: 0,
+        soundcloudLikeTrackLimit: 200,
+    },
+});
+
+bot.variables({
+    invite: "0"
+   })
+
+bot.status({
+    name: "Neco BETA",
+    type: "STREAMING",
+    URL: "https://youtube.com/",
+    time: 12
+  
+});
+
+
+bot.command({
+  name: "rejoindre",
+  code: `$joinvc`
+  
+});
+
+
+
+bot.command({
+
+  name: "createplay",
+
+  code: `$createApplicationCommand[global;Jouer;Starts playing music in the current connected channel;true;slash;[{
+
+    "name": "music-name",
+
+    "description": "The name of the music/Video that will be played",
+
+    "required": true,
+
+    "type": 3
+
+  }]]
+
+  Created the slash command.
+
+`
+
+});
+
+bot.command({
+name: "jouer",
+
+code: `$playTrack[$message;youtube]
+
+$title[Son joué : $message (Envoie .infomusique pour connaitre le nom exact)]
+
+  $color[#F9826D]`
+});
+
+bot.command({
+    name: "infomusique",
+    code: `
+Nom de la musique : $songInfo[title]
+
+Durée : $math[$songInfo[duration]/60] Seconds ($math[$songInfo[duration]/60/60/60] minutes)
+
+Arstist/e : $songInfo[artist]
+
+URL : <https://www.youtube.com/watch?v=$songInfo[id]>`})
+
+
+
+bot.command({
+  name: "passer",
+  code: `$skipTrack $title[↪️ Musique passée.]`
+});
+
+bot.command({
+   name: "volume",
+   code: ` $volume[$message]
+$title[🔊 Volume Réglé Sur $message]
+$description[Ce n'est pas permanent, si le bot quitte et reviens en vocal, le volume se remettera a 100.]
+$footer[Neco] $color[#F9826D]`,
+});
+
+bot.command({
+    name: "repeter",
+    code: `$loopMode[song]
+$title[🔁 Musique actuelle désormais jouée en boucle ]`});
+
+bot.command({
+    name: "stop",
+    code: `$loopMode[none] $title[➡️ La musique actuelle n'est plus jouée en boucle.]`});
+
+
+
+bot.command({
+    name: `leave`,
+    code: `$leaveVC $title[🔇 Le vocal a été quitté! Au revoir 💝]`});
+
+bot.command({
+    name: "viporder",
+    code: `
+$onlyForChannels[1173553198162792448;You can only execute this command in the <#1173553198162792448> channel.
+]
+
+$deletecommand
+$channelSendMessage[1173559262992138271;<@$authorID> Has ordered $message ||<@967883624135397442>||]
+$title[<:VIP:1173549615333769257>** $username , Your order has been delivered!**<:VIP:1173549615333769257>]
+$deleteIn[5]
+`})
+
+bot.command({
+    name: "blacklist",
+    code: `
+$title[⚠️**BLACKLIST**⚠️]
+$description[
+*BLACKLIST IS THE UNFINDABLE ACCOUNTS. DO NOT ASK FOR THEM IN ANY WAY.*
+<a:wall:1175566370277171293> SPOTIFY
+<a:wall:1175566370277171293> PLAYSTATION NETWORK (Psn)
+]`})
+
+bot.command({
+    name: "bl",
+    code:`$onlyForRoles[1173414003360411778;blud is not admin😳]
+$ban[$guildID;$mentioned[1]]
+banned the fuck out of <@$mentioned[1]>
+`})
